8c2b538e4ce15593664f12cc6df617a517b52f09
Andrew25ngoCSUF <148040377+Andrew25ngoCSUF@users.noreply.github.com>
2023-10-22 22:08:41 -0700

    fixed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Signin from "./components/Signin/Signin";
+import FoodDeliveryApp from "./components/TitanEats/TitanEats";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/titan-eats" element ={<TitanEats />} />
       </Routes>
     </Router>
   );
